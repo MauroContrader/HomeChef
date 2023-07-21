6bc2e7827e3f3811bef1b9c12c87fc4111283d87
Mauro <112409884+MauroContrader@users.noreply.github.com>
2023-07-21 11:39:16 +0200

    Add files via upload

---
+++ HomeChef/src/main/java/Mauro/HomeChef/HomeChefApplication.java
@@ -0,0 +1,13 @@
+package Mauro.HomeChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeChefApplication.class, args);
+	}
+
+}
---
+++ HomeChef/src/test/java/Mauro/HomeChef/HomeChefApplicationTests.java
@@ -0,0 +1,13 @@
+package Mauro.HomeChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
